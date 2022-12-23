a4a34fab24848a2d00db587389cc1a9396e7cbfb
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-23 13:54:28 -0800

    pass test for all 8 planetary age conversions.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -5,5 +5,12 @@ describe('PlanetaryAge', () => {
     let age = 56;
     const planetaryAge = new PlanetaryAge(age);
     expect(planetaryAge.getPlanetaryAge(0, age)).toEqual(233.33);
+    expect(planetaryAge.getPlanetaryAge(1, age)).toEqual(90.32);
+    expect(planetaryAge.getPlanetaryAge(2, age)).toEqual(56);
+    expect(planetaryAge.getPlanetaryAge(3, age)).toEqual(29.79);
+    expect(planetaryAge.getPlanetaryAge(4, age)).toEqual(4.72);
+    expect(planetaryAge.getPlanetaryAge(5, age)).toEqual(1.90);
+    expect(planetaryAge.getPlanetaryAge(6, age)).toEqual(.67)
+    expect(planetaryAge.getPlanetaryAge(7, age)).toEqual(.34);
   })
 })
