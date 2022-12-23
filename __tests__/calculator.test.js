3970826cfe1248cc9efa222a1882eb4e7cc9a04c
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-23 13:49:47 -0800

    commit for first passing test for Mercurial age conversion.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,9 +1,9 @@
 import {PlanetaryAge} from '../src/js/calculator.js';
 
 describe('PlanetaryAge', () => {
-  test('should correctly return user age on each of the 7 planets', () => {
+  test('should correctly return user age on each of the 8 planets', () => {
     let age = 56;
     const planetaryAge = new PlanetaryAge(age);
-    expect((planetaryAge.getPlanetaryAge(0, age))).toEqual(233.33);
+    expect(planetaryAge.getPlanetaryAge(0, age)).toEqual(233.33);
   })
 })
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,12 @@
 export class PlanetaryAge {
+  static PlanetsAndRotations = ([['Mercury', .24], ['Venus', .62], ['Earth', 1], ['Mars', 1.88], ['Jupiter', 11.86], ['Saturn', 29.46], ['Uranus', 84.01], ['Neptune', 164.79]]);
+
+  constructor(age) {
+    this.age = age;
+  }
+
   getPlanetaryAge(i, age) {
-    
+    let planetAge = (age / PlanetaryAge.PlanetsAndRotations[i][1]).toFixed(2);
+    return parseFloat(planetAge);
   }
 }
